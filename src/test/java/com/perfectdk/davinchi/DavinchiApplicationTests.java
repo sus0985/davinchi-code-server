823090d1f616523e12f777f309b17db2083f1566
edd <edd@vlending.co.kr>
2023-12-30 23:15:40 +0900

    프로젝트 시작

---
+++ src/test/java/com/perfectdk/davinchi/DavinchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfectdk.davinchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavinchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
