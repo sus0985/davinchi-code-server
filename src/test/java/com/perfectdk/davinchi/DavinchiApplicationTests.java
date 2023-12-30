c6165a01bae2b4d0285a2db778886763baa9baea
edd <edd@vlending.co.kr>
2023-12-31 00:03:26 +0900

    테스트 삭제

--- src/test/java/com/perfectdk/davinchi/DavinchiApplicationTests.java
+++ src/test/java/com/perfectdk/davinchi/DavinchiApplicationTests.java
@@ -1,12 +1,11 @@
 package com.perfectdk.davinchi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DavinchiApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
